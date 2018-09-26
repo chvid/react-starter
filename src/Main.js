1cd4ab4f66a774b1202f493e6fad485c43863782
Christian Hvid <christian.hvid@gmail.com>
2018-09-26 17:38:42 +0200

    Using font-awesome react package.

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { formatAmount } from './utils.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBolt } from '@fortawesome/free-solid-svg-icons'
 
 export class Main extends React.Component {
     constructor() {
@@ -24,31 +26,31 @@ export class Main extends React.Component {
                     <div className="columns">
                         <div className="column">
                             <h2 className="subtitle">
-                                <span className="icon has-text-info"><i className="fa fa-bolt"></i></span>
+                                <span className="icon has-text-info is-medium"><FontAwesomeIcon icon={faBolt} size="sm"/></span>
                                 Your Current Finances
                             </h2>
                             <form>
                                 <div className="field">
                                     <label className="label">Monthly Income</label>
                                     <div className="control">
-                                        <input 
-                                            className="input" 
-                                            type="text" 
-                                            placeholder="Monthly Income ..." 
-                                            value={this.state.monthlyIncome} 
-                                            onChange={(e) => this.setState({monthlyIncome: e.target.value})}
+                                        <input
+                                            className="input"
+                                            type="text"
+                                            placeholder="Monthly Income ..."
+                                            value={this.state.monthlyIncome}
+                                            onChange={(e) => this.setState({ monthlyIncome: e.target.value })}
                                         />
                                     </div>
                                 </div>
                                 <div className="field">
                                     <label className="label">Monthly Expenses</label>
                                     <div className="control">
-                                        <input 
-                                            className="input" 
-                                            type="text" 
-                                            placeholder="Monthly Expenses ..." 
-                                            value={this.state.monthlyExpenses} 
-                                            onChange={(e) => this.setState({monthlyExpenses: e.target.value})}
+                                        <input
+                                            className="input"
+                                            type="text"
+                                            placeholder="Monthly Expenses ..."
+                                            value={this.state.monthlyExpenses}
+                                            onChange={(e) => this.setState({ monthlyExpenses: e.target.value })}
                                         />
                                     </div>
                                 </div>
@@ -56,28 +58,28 @@ export class Main extends React.Component {
                         </div>
                         <div className="column">
                             <h2 className="subtitle">
-                                <span className="icon has-text-success"><i className="fa fa-home"></i></span>
+                                <span className="icon has-text-success is-medium"><FontAwesomeIcon icon={faHome} size="sm"/></span>
                                 Your Future Finances
                             </h2>
                             <p>You may buy a house for:</p>
                             <p>&nbsp;</p>
-                            { 
+                            {
                                 youMayLoan > 0 &&
                                 <div className="notification is-primary">
                                     <h1 className="title">{formatAmount(youMayLoan, 0)}</h1>
                                 </div>
                             }
-                            { youMayLoan > 10000000 && <p>Because you are very rich.</p> }
-                            { 
+                            {youMayLoan > 10000000 && <p>Because you are very rich.</p>}
+                            {
                                 youMayLoan == 0 &&
                                 <div className="notification is-warning">
                                     <h1 className="title">NOTHING !!!!</h1>
                                 </div>
                             }
-                            { youMayLoan < 100000 && <p>Because you are very poor.</p> }
+                            {youMayLoan < 100000 && <p>Because you are very poor.</p>}
 
-                            { 
-                                isNaN(youMayLoan) && 
+                            {
+                                isNaN(youMayLoan) &&
                                 <div className="notification is-danger">HEY! Rubbish won&#39;t help you at the bank.</div>
                             }
                         </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bulma';
-import 'font-awesome/css/font-awesome.css';
 
 import './index.css';
 
