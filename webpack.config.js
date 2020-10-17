cf238eb4d1770c48f2e6d2ec6cff49f3085e57e0
Christian Hvid <christian.hvid@gmail.com>
2020-10-17 08:25:36 +0200

    Upgraded to webpack 5.

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const src = Path.resolve(__dirname, './src');
-const dist = Path.resolve(__dirname, 'dist');
+const dist = Path.resolve(__dirname, './dist');
 
 const config = {
+    devtool: false,
     entry: Path.resolve(src, 'index.js'),
     output: {
         path: dist,
@@ -13,14 +14,14 @@ const config = {
     },
     module: {
         rules: [
-            { test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.(scss|sass)$/, loader: 'style-loader!css-loader!sass-loader' },
-            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' }, 
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' }, 
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+            { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(scss|sass)$/, use: ['style-loader','css-loader','sass-loader'] },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
         ]
     },
     plugins: [new HtmlWebpackPlugin({ template: 'index.html' }), new CopyWebpackPlugin([{ from: 'images', to: 'images' }])],
